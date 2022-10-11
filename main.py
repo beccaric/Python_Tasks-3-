8e3e6facf04b9296b34b8d51bb4698671279afde
becca <93043262+beccaric@users.noreply.github.com>
2022-10-11 15:58:34 +0000

    subprograms

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ def UserName ():
   print("Your name is",name)
 UserName()
 
+def numbers (): #defines the function
+  num1=int(input("Please enter a number"))
+  num2=int(input("Please enter a second number"))
+  num3=int(input("Please enter a third number"))
+  num4=int(input("Please enter a fourth number"))
+  sumnum=num1+num2+num3+num4
+  print("The sum of the numbers you entered is", sumnum)
+numbers()
